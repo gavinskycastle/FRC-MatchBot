19936443368e48f7614a23855f474976dd9586bd
Gavin P <gavinjpeeler@gmail.com>
2023-03-26 11:18:30 -0700

    Added local files

---
+++ main.py
@@ -0,0 +1,107 @@
+import discord
+import datetime
+from discord import app_commands
+from discord.ext import commands
+from statbotics import Statbotics
+from os import environ
+from utils import *
+
+# Setting channel ids
+BOT_TESTING_CHANNEL_ID = 1031012980692889615
+CONEUNIST_CHANNEL_ID = 1040069639847301120
+CHANNEL_ID = BOT_TESTING_CHANNEL_ID
+
+# Get the token from the environment variables
+TOKEN = environ['MATCH_BOT_DISCORD_TOKEN']
+
+# Getting current year. By default, only matches from the current season are listed
+year = int(datetime.date.today().strftime("%Y"))
+
+# Initalize Statbotics
+sb = Statbotics()
+
+# TODO: Determine required intents
+intents = discord.Intents.all()
+
+# Initialize our bot
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+# Runs when bot connects
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    try:
+        synced = await bot.tree.sync()
+        print(f"Synced {len(synced)} command(s)")
+    except Exception as e:
+        print(e)
+
+# Basic ping command
+@bot.tree.command(name="ping")
+async def ping(interaction: discord.Interaction):
+    await interaction.response.send_message("Pong!")
+
+@bot.tree.command(name="nextmatch")
+@app_commands.describe(type = "Displays the next match the team will play in or the next match in the event", id = "The team/event number or id")
+async def next_match(interaction: discord.Interaction, type: str, id: str):
+    response = "`Sending...`"
+    matches = get_team_or_event_matches(type, id, year)
+    if matches == {}:
+        response = "`Error: Must input valid team or event`"
+    
+    await interaction.response.send_message(response)
+    
+    if matches != {}:
+        header = "**Next upcoming match for "
+        if type == "team":
+            header = header + format_team_header(int(id)) + ":**"
+        if type == "event":
+            header = header + "the " + format_event_header(id) + ":**"
+        print_out = split_string_under_2000(get_match_table(get_next_match(matches)))
+        await bot.get_channel(CHANNEL_ID).send(header)
+        for out in print_out:
+            await bot.get_channel(CHANNEL_ID).send(code_block(out))
+
+@bot.tree.command(name="recentmatch")
+@app_commands.describe(type = "Displays the last played match for the team or in the event, as well as the match video if posted", id = "The team/event number or id")
+async def recent_match(interaction: discord.Interaction, type: str, id: str):
+    response = "`Sending...`"
+    matches = get_team_or_event_matches(type, id, year)
+    if matches == {}:
+        response = "`Error: Must input valid team or event`"
+    
+    await interaction.response.send_message(response)
+    
+    if matches != {}:
+        header = "**Most recent match for "
+        if type == "team":
+            header = header + format_team_header(int(id)) + ":**"
+        if type == "event":
+            header = header + "the " + format_event_header(id) + ":**"
+        print_out = split_string_under_2000(get_match_table(get_most_recent_match(matches)))
+        await bot.get_channel(CHANNEL_ID).send(header)
+        for out in print_out:
+            await bot.get_channel(CHANNEL_ID).send(code_block(out))
+
+@bot.tree.command(name="allmatches")
+@app_commands.describe(type = "Displays all matches played by a team or in an event", id = "The team/event number or id")
+async def all_matches(interaction: discord.Interaction, type: str, id: str):
+    response = "`Sending...`"
+    matches = get_team_or_event_matches(type, id, year)
+    if matches == {}:
+        response = "`Error: Must input valid team or event`"
+    
+    await interaction.response.send_message(response)
+    
+    if matches != {}:
+        header = "**All matches for "
+        if type == "team":
+            header = header + format_team_header(int(id)) + ":**"
+        if type == "event":
+            header = header + "the " + format_event_header(id) + ":**"
+        print_out = split_string_under_2000(get_match_table(matches))
+        await bot.get_channel(CHANNEL_ID).send(header)
+        for out in print_out:
+            await bot.get_channel(CHANNEL_ID).send(code_block(out))
+
+bot.run(TOKEN)
---
+++ utils.py
@@ -0,0 +1,106 @@
+from statbotics import Statbotics
+from prettytable import PrettyTable
+
+sb = Statbotics()
+
+def get_most_recent_match(matches: list):
+    recent_match = {}
+    for match in matches[::-1]:
+        if match["status"] == "Completed":
+            recent_match = match
+            break
+    return recent_match
+
+def get_next_match(matches: list):
+    next_match = {}
+    for match in matches:
+        if match["status"] != "Completed":
+            next_match = match
+            break
+    return next_match
+
+def get_match_name(match: dict):
+    semifinal = False
+    if match["comp_level"] == "qm":
+        match_name = "Qualification "
+    elif match["comp_level"] == "qf":
+        match_name = "Quarterfinal "
+    elif match["comp_level"] == "sf":
+        match_name = "Semifinal "
+        semifinal = True
+    elif match["comp_level"] == "f":
+        match_name = "Final "
+    else:
+        match_name = ""
+    
+    if semifinal:
+        match_name = match_name + str(match["set_number"]) + " Match "
+    match_name = match_name + str(match["match_number"])
+    
+    return match_name
+
+def add_match_to_table(match: dict, table):
+    try:
+        match_event_name = sb.get_event(event=match["event"])["name"]
+        match_name = get_match_name(match)
+        
+        red_score = match["red_score"]
+        blue_score = match["blue_score"]
+        winner = match["winner"].title()
+        
+        if red_score == -1 or blue_score == -1:
+            red_score = "TBD"
+            blue_score = "TBD"
+            winner = "TBD"
+        
+        table.add_row([match_event_name, match_name, match["red_1"], match["red_2"], match["red_3"], match["blue_1"], match["blue_2"], match["blue_3"], red_score, blue_score, winner])
+    except KeyError:
+        table.add_row(["TBD", "TBD", "TBD", "TBD", "TBD", "TBD", "TBD", "TBD", "TBD", "TBD", "TBD"])
+
+def get_match_table(matches):
+    table = PrettyTable()
+    table.field_names = ["Event", "Match", "Red 1", "Red 2", "Red 3", "Blue 1", "Blue 2", "Blue 3", "Red Score", "Blue Score", "Winner"]
+    if type(matches) is list:
+        for match in matches:
+            add_match_to_table(match, table)
+    elif type(matches) is dict:
+        add_match_to_table(matches, table)
+    return table.get_string()
+
+def code_block(string):
+    return "```"+string+"```"
+
+def get_team_or_event_matches(type, id, year):
+    if type == "team":
+        try:
+            return sb.get_matches(team=int(id), year=year)
+        except Exception:
+            return {}
+    elif type == "event":
+        try:
+            return sb.get_matches(event=id)
+        except Exception:
+            return {}
+    else:
+        return {}
+
+# If a string is too long to be outputted as one message, split it into strings such that it never reaches the 2000 character limit.
+def split_string_under_2000(string):
+    lines = string.splitlines()
+    split_strings = []
+    split_string = ""
+    for line in lines:
+        if len(split_string) > 1800:
+            split_strings.append(split_string)
+            split_string = ""
+        split_string = split_string + line + "\n"
+    split_strings.append(split_string)
+    return split_strings
+
+def format_team_header(team: int):
+    team_name = sb.get_team(team=team)["name"]
+    return f"Team {str(team)} ({team_name})"
+
+def format_event_header(event: str):
+    event_name = sb.get_event(event=event)["name"]
+    return event_name
